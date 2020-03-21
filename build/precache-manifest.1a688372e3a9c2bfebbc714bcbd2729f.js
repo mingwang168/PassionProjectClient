80c501a6fcd8e26ed86f4ef7ad3b1cd0e018a2f9
mingwang168 <mingwang0904@gmail.com>
2020-03-20 20:47:20 -0700

    add build

---
+++ build/precache-manifest.1a688372e3a9c2bfebbc714bcbd2729f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4071039f1f963e22b8f38bec9287828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1820d4f63d6ff8b5837c",
+    "url": "/static/css/2.1d10ae30.chunk.css"
+  },
+  {
+    "revision": "fedf2a3742e717cfcbc4",
+    "url": "/static/css/main.15610220.chunk.css"
+  },
+  {
+    "revision": "1820d4f63d6ff8b5837c",
+    "url": "/static/js/2.b797873f.chunk.js"
+  },
+  {
+    "revision": "7fb7ad87ffe5def248cda590c2e9177b",
+    "url": "/static/js/2.b797873f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fedf2a3742e717cfcbc4",
+    "url": "/static/js/main.cf87ca39.chunk.js"
+  },
+  {
+    "revision": "a77dbc871a578b4846d9",
+    "url": "/static/js/runtime-main.c8457b1a.js"
+  },
+  {
+    "revision": "b9d2f06a74f06408969172fe3d7618b6",
+    "url": "/static/media/English-words-vocabulary.b9d2f06a.jpg"
+  },
+  {
+    "revision": "d67b624e7924ab33243e808038ed9c97",
+    "url": "/static/media/background.d67b624e.png"
+  },
+  {
+    "revision": "4608d02c877fc22ad189780e6b7aaf32",
+    "url": "/static/media/clock1.4608d02c.png"
+  }
+]);
